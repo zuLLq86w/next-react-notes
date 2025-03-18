3a7d645cb855b8d5a677c331603fe03f1d9576d2
爱吃猫的鱼 <zullq86w@gmail.com>
2025-03-18 16:18:36 +0800

    侧边栏笔记列表

---
+++ components/SidebarNoteItem.js
@@ -0,0 +1,20 @@
+import SidebarNoteItemContent from "./SidebarNoteItemContent";
+import SidebarNoteItemHeader from "./SidebarNoteItemHeader";
+
+export default function SidebarNoteItem({noteId, note}){
+  const {title, content = '', updateTime} = note;
+  return (
+    <SidebarNoteItemContent
+      id={noteId}
+      title={note.title}
+      expandedChildren={
+        <p className="sidebar-note-excerpt">
+          {content.substring(0, 20) || <i>(No content)</i>}
+        </p>
+      }
+    >
+      <SidebarNoteItemHeader title={title} updateTime={updateTime}></SidebarNoteItemHeader>
+
+    </SidebarNoteItemContent>
+  );
+}
--- components/SidebarNoteList.js
+++ components/SidebarNoteList.js
@@ -1,27 +1,24 @@
-import dayjs from "dayjs";
+import SidebarNoteItem from '@/components/SidebarNoteItem';
+import { getAllNotes } from '@/lib/redis';
 
-export default async function SidebarNoteList({notes}) {
+export default async function NoteList() {
+  // const sleep = ms => new Promise(r => setTimeout(r, ms));
+  // await sleep(10000);
+  const notes = await getAllNotes()
+
   const arr = Object.entries(notes);
 
   if (arr.length == 0) {
-    return (<div className="notes-empty">
+    return <div className="notes-empty">
       {'No notes created yet!'}
-    </div>);
+    </div>
   }
 
-  return (
-    <ul className="notes-list">
-      { arr.map(([noteId, note]) => {
-        const {title, updateTime} = JSON.parse(note);
-        return (
-          <li key={noteId}>
-            <header className="sidebar-note-header">
-              <strong>{title}</strong>
-              <small>{dayjs(updateTime).format('YYYY-MM-DD hh:mm:ss')}</small>
-            </header>
-          </li>
-        )
-      })}
-    </ul>
-  );
+  return <ul className="notes-list">
+    {arr.map(([noteId, note]) => {
+      return <li key={noteId}>
+        <SidebarNoteItem noteId={noteId} note={JSON.parse(note)} />
+      </li>
+    })}
+  </ul>
 }
